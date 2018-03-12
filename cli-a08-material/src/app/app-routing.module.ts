4351321b102068eb140d4b5018ce29a7b00775a1
pepesan <pepesan@gmail.com>
2018-03-12 16:40:01 +0100

    Mejoras en la presentación de material de la sidebar

--- cli-a08-material/src/app/app-routing.module.ts
+++ cli-a08-material/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioComponent } from './inicio/inicio.component';
 
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent
+  },
+  {
+    path: 'acerca-de',
+    component: AcercaDeComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
