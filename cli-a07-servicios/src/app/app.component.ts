49ca2e162118d1a8c5040e72fea864cddabaffb8
pepesan <pepesan@gmail.com>
2018-03-15 12:21:45 +0100

    ejemplo de uso de datos compartidos con un servicio

--- cli-a07-servicios/src/app/app.component.ts
+++ cli-a07-servicios/src/app/app.component.ts
@@ -14,21 +14,41 @@ export class AppComponent implements OnInit, OnDestroy{
 
 
   title = 'app';
-  data:any;
+  promesaDatos:any;
   libros:any;
   datos:any;
+  logged:Observable<boolean>;
+  login:boolean;
   constructor(private miServicio: MiServicioService, public datosService: DatosService){
     this.datos=this.datosService.getDatos();
     console.log(this.datos);
-    this.data=this.miServicio.getData();
-    console.log(this.data);
+    this.promesaDatos=this.miServicio.getData();
+    console.log(this.promesaDatos);
+      this.login=false;
+    this.logged=this.miServicio.getLogged();
+      this.logged.subscribe(
+        this.actualizaLogin.bind(this);
+      );
+  }
+  actualizaLogin(datos){
+      this.login=datos;
   }
   ngOnInit(){
-    this.data.subscribe(this.muestraLibros.bind(this));
+    this.promesaDatos.subscribe(this.muestraLibros.bind(this));
   }
   muestraLibros(datos){
     console.log(datos);
     this.libros=datos;
+  }
+  loguear(){
+        console.log("logueando");
+        this.miServicio.setLogged(true);
+        //this.logged=this.miServicio.getLogged();
+  }
+  desloguear(){
+      console.log("deslogueando");
+        this.miServicio.setLogged(false);
+        //this.logged=this.miServicio.getLogged();
   }
   ngOnDestroy(): void {
     //throw new Error("Method not implemented.");
--- cli-a07-servicios/src/app/mi-servicio.service.ts
+++ cli-a07-servicios/src/app/mi-servicio.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from "rxjs/Observable";
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class MiServicioService {
 
-  data:any;
+  promesaDatos:any;
   url:string;
+  private loginSource = new BehaviorSubject<string>(false);
+  currentLogin = this.loginSource.asObservable();
 
   constructor(private http:HttpClient) {
     this.url="http://www.mocky.io/v2/5a54ae822d00005f235b1cd2";
-    this.data=this.http.get(this.url);
+    this.promesaDatos=this.http.get(this.url);
   }
-  getData(): Observable<any>{
-    return this.data;
+  getData(): Observable<boolean>{
+    return this.promesaDatos;
+  }
+  getLogged(){
+      return this.currentLogin;
+  }
+  setLogged(login){
+      this.loginSource.next(login)
   }
 
 }
